0ac0cacb2c067cab05ec729e3d0c2fa5b86baead
lesteven <le_steven@outlook.com>
2019-02-12 12:39:21 -0800

    add more parse tests

--- test/parse.js
+++ test/parse.js
@@ -4,6 +4,16 @@ const qsarr = require('../src/query-string-arr')
 const { parse } = qsarr
 
 describe('parse', () => {
+  it('returns empty array if not string', (done) => {
+    const notStr = 123
+    assert.deepStrictEqual(parse(notStr), [])
+    done()
+  })
+  it('returns empty array if string = ?', (done) => {
+    const str = '?'
+    assert.deepStrictEqual(parse(str), [])
+    done()
+  })
   it('turns string to array', (done) => {
     const arr = [['a', 1]]
     const str = '?a=1'
