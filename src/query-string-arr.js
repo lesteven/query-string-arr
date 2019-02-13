f7ef12f4dc5aace8bf9bd46b469ca5cabb224873
lesteven <le_steven@outlook.com>
2019-02-12 16:06:47 -0800

    finish parse

--- src/query-string-arr.js
+++ src/query-string-arr.js
@@ -1,6 +1,94 @@
+const strictUriEncode = require('strict-uri-encode')
+const decodeComponent = require('decode-uri-component')
 
-function parse(input, options) {
-  return [[]]
+// code heavily borrowed from https://github.com/sindresorhus/query-string
+
+function parserForArrayFormat() {
+  return (key, value, accumulator) => {
+    if (accumulator[key] === undefined) {
+      accumulator[key] = value
+      return
+    }
+
+    accumulator[key] = [].concat(accumulator[key], value)
+  }
+}
+
+function encode(value, options) {
+  if (options.encode) {
+    return options.strict ? strictUriEncode(value) : encodeURIComponent(value)
+  }
+
+  return value
+}
+
+function encoderForArrayFormat(options) {
+  return (key, value) => (value === null ? encode(key, options) : [
+    encode(key, options),
+    '=',
+    encode(value, options),
+  ].join(''))
+}
+
+function decode(value, options) {
+  if (options.decode) {
+    return decodeComponent(value)
+  }
+
+  return value
+}
+
+function parse(input, type) {
+  const options = Object.assign({ decode: true, arrayFormat: 'none' })
+
+  const formatter = parserForArrayFormat(options)
+
+  const ret = {}
+  if (typeof input !== 'string') {
+    if (type !== undefined && type.array) return []
+    return ret
+  }
+
+  input = input.trim().replace(/^[?#&]/, '')
+
+  if (!input) {
+    if (type !== undefined && type.array) return []
+    return ret
+  }
+
+  const keyOrder = []
+  for (const param of input.split('&')) {
+    let [key, value] = param.replace(/\+/g, ' ').split('=')
+
+    // Missing `=` should be `null`:
+    // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+    value = value === undefined ? null : decode(value, options)
+    key = decode(key, options)
+    if (!ret.key) {
+      keyOrder.push(key)
+    }
+    formatter(key, value, ret)
+  }
+
+  if (type !== undefined && type.array) {
+    const arr = []
+    for (key of keyOrder) {
+      arr.push([key, ret[key]])
+    }
+    return arr
+  }
+
+  return Object.keys(ret).sort().reduce((result, key) => {
+    const value = ret[key]
+    if (Boolean(value) && typeof value === 'object' && !Array.isArray(value)) {
+      // Sort object keys, not values
+      result[key] = keysSorter(value)
+    } else {
+      result[key] = value
+    }
+
+    return result
+  }, Object.create(null))
 }
 
 exports.parse = parse
--- test/parse.js
+++ test/parse.js
@@ -3,27 +3,52 @@ const qsarr = require('../src/query-string-arr')
 
 const { parse } = qsarr
 
-describe('parse', () => {
+describe('parse to array', () => {
   it('returns empty array if not string', (done) => {
     const notStr = 123
-    assert.deepStrictEqual(parse(notStr), [])
+    assert.deepStrictEqual(parse(notStr, { array: true }), [])
     done()
   })
   it('returns empty array if string = ?', (done) => {
     const str = '?'
-    assert.deepStrictEqual(parse(str), [])
+    assert.deepStrictEqual(parse(str, { array: true }), [])
     done()
   })
   it('turns string to array', (done) => {
-    const arr = [['a', 1]]
+    const arr = [['a', '1']]
     const str = '?a=1'
-    assert.deepStrictEqual(parse(str), arr)
+    assert.deepStrictEqual(parse(str, { array: true }), arr)
     done()
   })
   it('turns string to array 2 values', (done) => {
-    const arr = [['z', 10], ['a', 1]]
+    const arr = [['z', '10'], ['a', '1']]
     const str = '?z=10&a=1'
-    assert.deepStrictEqual(parse(str), arr)
+    assert.deepStrictEqual(parse(str, { array: true }), arr)
+    done()
+  })
+})
+
+describe('parse to object', () => {
+  it('returns empty object if not string', (done) => {
+    const notStr = 123
+    assert.deepStrictEqual(parse(notStr), {})
+    done()
+  })
+  it('returns empty object if string = ?', (done) => {
+    const str = '?'
+    assert.deepStrictEqual(parse(str), {})
+    done()
+  })
+  it('turns string to object', (done) => {
+    const obj = { a: '1' }
+    const str = '?a=1'
+    assert.deepEqual(parse(str), obj)
+    done()
+  })
+  it('turns string to object 2 values', (done) => {
+    const obj = { a: '1', z: '10' }
+    const str = '?z=10&a=1'
+    assert.deepEqual(parse(str), obj)
     done()
   })
 })
